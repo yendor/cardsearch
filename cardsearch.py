53ff74f8e49ae60ac7b9fecafc248c1ecd5f7157
Rodney Amato <rodnet@gmail.com>
2010-07-11 16:54:58 +1000

    Fix an issue where the output file wasnt being whitelisted if it had a relative path. Also adjust the file scanning to use less memory and cpu

--- cardsearch.py
+++ cardsearch.py
@@ -6,35 +6,31 @@ class CardSearch:
     def __init__(self):
          self.output_filename = "/var/log/cardsearch.log"
          self.start_path = "/"
-         self.whitelist_filenames = [self.output_filename]
+
+         shortargs = 'o:f:d:'
+         longargs = ['output=', 'config=', 'start=']
+
+         options, remainder = getopt.getopt(sys.argv[1:], shortargs, longargs)
+
+         for opt, arg in options:
+             if opt in ('-o', '--output'):
+                 self.output_filename = os.path.abspath(arg)
+             elif opt in ('-f', '--config'):
+                 self.config_filename = arg
+             elif opt in ('-d', '--start'):
+                 self.start_path = os.path.abspath(arg)
+
+         self.whitelist_filenames = [self.output_filename, "/proc", "/dev"]
 
     def search(self):
-        shortargs = 'o:f:d:'
-        longargs = ['output=', 'config=', 'start=']
-
-        options, remainder = getopt.getopt(sys.argv[1:], shortargs, longargs)
-
-        for opt, arg in options:
-            if opt in ('-o', '--output'):
-                self.output_filename = arg
-            elif opt in ('-f', '--config'):
-                self.config_filename = arg
-            elif opt in ('-d', '--start'):
-                self.start_path = arg
-
-        start_path = os.path.abspath(self.start_path)
-
-        if os.path.isdir(start_path):
+        if os.path.isdir(self.start_path):
             try:
                 self.output_file = open(self.output_filename, 'w')
             except IOError:
                 print >> sys.stderr, "Can't open log file %s for writing" % (self.output_filename)
                 sys.exit(1)
 
-            self.output_file.write("OPTIONS   : %s\n" % (options))
-            self.output_file.write("REMAINDER : %s\n" % (remainder))
-
-            self.walk(start_path)
+            self.walk(self.start_path)
         else:
             print "Starting directory %s does not exist, exiting." % (self.start_path)
             sys.exit(1)
@@ -46,6 +42,7 @@ class CardSearch:
 
                 if path in self.whitelist_filenames:
                     print >> sys.stderr, "Skipping %s due to whitelisting" % (path)
+                    continue
 
                 if os.path.isfile(path):
                     self.check(path)
@@ -57,24 +54,25 @@ class CardSearch:
     def check(self, filepath):
         try:
             f = open(filepath, 'r')
-            contents = f.read();
 
-            cardpattern = re.compile('\d{12,19}')
+            for line in f:
+                cardpattern = re.compile('\d{12,19}')
 
-            matches = cardpattern.findall(contents)
+                matches = cardpattern.findall(line)
 
-            confirmed_matches = []
+                confirmed_matches = []
 
-            if matches:
-                for match in matches:
-                    if (is_luhn_valid(match)):
-                        confirmed_matches.append(match)
+                if matches:
+                    for match in matches:
+                        if (is_luhn_valid(match)):
+                            confirmed_matches.append(match)
 
-            if confirmed_matches:
-                self.output_file.write("Found %d matches in %s\n" % (len(confirmed_matches), filepath))
-                for match in confirmed_matches:
-                    self.output_file.write("%s\n" % (match))
-            usleep(10)
+                if confirmed_matches:
+                    self.output_file.write("Found %d matches in %s\n" % (len(confirmed_matches), filepath))
+                    for match in confirmed_matches:
+                        self.output_file.write("%s\n" % (match))
+                usleep(1)
+
 
         except IOError:
             print >> sys.stderr, "Can't read %s" % (filepath)
